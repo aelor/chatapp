f5bc96d1186a8fa8d9141b638251b0e821cf8e0c
Rituraj <rituraj@kreatio.com>
2013-08-07 15:16:39 +0530

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Tenthapp::Application.config.secret_key_base = '96955040104ee5f751f96a4640e90a95d819774b949125db7ce4c377a287a0d21e83b7b574ed6a59b8bbd797841e2642e4adfa6c88a18c2bc4ef304214b91c73'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Tenthapp::Application.config.session_store :cookie_store, key: '_tenthapp_session'
