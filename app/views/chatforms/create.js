c2378200eb8daec2257ee65f2818eb72f79ca939
Rituraj <rituraj@kreatio.com>
2013-08-12 20:05:02 +0530

    css added

--- app/views/chatforms/create.js
+++ app/views/chatforms/create.js
@@ -3,5 +3,6 @@ $("#texts").append("<%= escape_javascript render(@chatform) %>");
 $("li:first").slideUp("normal", function() { 
 					    	$(this).remove(); 
 					    	});
-$("#new_chatform")[0].reset();
+
 <% end %>
+$("#new_chatform")[0].reset();
